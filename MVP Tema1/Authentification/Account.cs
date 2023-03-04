5ca942a48e5459545a7fea87c0041133fb60df32
AlexUnguranu <a.ungureanu0099@gmail.com>
2023-03-04 16:26:05 +0200

    added a class to store accounts

---
+++ MVP Tema1/Authentification/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MVP_Tema1.Authentification
+{
+    internal class Account
+    {
+        public string Username { get; private set; }
+
+        //public string Password { get; private set; }
+
+        public string AvatarPath { get; private set; }
+
+        public Account(string username, /*string password, */string avatarPath)
+        {
+            if (username == string.Empty)
+            {
+                throw new ArgumentException("Username cannot be empty!", nameof(username));
+            }
+            //if (password == string.Empty)
+            //{
+            //    throw new ArgumentException("Password cannot be empty!", nameof(password));
+            //}
+            //Password = password;
+            if (avatarPath == string.Empty)
+            {
+                throw new ArgumentException("Avatar path cannot be empty!", nameof(avatarPath));
+            }
+
+            Username = username;
+            AvatarPath = avatarPath;
+        }
+    }
+}
