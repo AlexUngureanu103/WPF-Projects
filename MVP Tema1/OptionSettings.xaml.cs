39db3d599523e7abdfedd273b424bceb79bcb617
AlexUnguranu <a.ungureanu0099@gmail.com>
2023-03-06 15:38:29 +0200

    Added a class to change the board Dimensions

---
+++ MVP Tema1/OptionSettings.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace MVP_Tema1
+{
+    /// <summary>
+    /// Interaction logic for Options.xaml
+    /// </summary>
+    public partial class OptionSettings : Window
+    {
+        public KeyValuePair<int, int> BoardDimensions { get; set; }
+        public OptionSettings()
+        {
+            InitializeComponent();
+            BoardDimensions = new KeyValuePair<int, int>(5, 5);
+        }
+
+        private void ClassicDimension_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CustomDimensions_Click(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(NoRows.Text, out int width) || !int.TryParse(NoColumns.Text, out int height))
+            {
+                MessageBox.Show("Invalid dimensions");
+                return;
+            }
+            if (width < 2 || height < 2)
+            {
+                MessageBox.Show("Dimensions must be at least 2x2");
+                return;
+            }
+            if (width > 10 || height > 10)
+            {
+                MessageBox.Show("Dimensions must be at most 10x10");
+                return;
+            }
+            BoardDimensions = new KeyValuePair<int, int>(width, height);
+            this.Close();
+        }
+    }
+}
