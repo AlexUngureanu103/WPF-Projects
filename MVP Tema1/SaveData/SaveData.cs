e90de3c45eed43481a5674e4776b6898de24bd89
AlexUnguranu <a.ungureanu0099@gmail.com>
2023-03-07 12:27:18 +0200

    Added a new class to handle saveData when the user desires

---
+++ MVP Tema1/SaveData/SaveData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace MVP_Tema1.SaveData
+{
+    public  class Data
+    {
+        private string FilePath;
+
+
+        public Data(string folderPath)
+        {
+            if (string.IsNullOrEmpty(folderPath))
+            {
+                throw new ArgumentException("Folder cannot be null or empty", nameof(folderPath));
+            }
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            this.FilePath = Path.Combine(folderPath, "Data.txt");
+            if (!File.Exists(FilePath))
+            {
+                File.Create(FilePath);
+            }
+        }
+    }
+}
