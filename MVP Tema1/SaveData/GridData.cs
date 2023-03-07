6d894bbb9b50459af33540aca30371b797d9c818
AlexUnguranu <a.ungureanu0099@gmail.com>
2023-03-07 16:59:35 +0200

    Added a class to save data inside files with

---
+++ MVP Tema1/SaveData/GridData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MVP_Tema1.SaveData
+{
+    public class GridData
+    {
+        public int level { get; set; }
+
+        public int rows { get; set; }
+
+        public int columns { get; set; }
+
+        public List<int> ImagesRow { get; set; }
+
+        public List<int> ImagesColumn { get; set; }
+
+        public List<string> ImagesPath { get; set; }
+
+    }
+}
