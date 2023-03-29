faa47ec5009ec6be2f9edf73d860eb03829b1bef
AlexUnguranu <a.ungureanu0099@gmail.com>
2023-03-29 17:50:18 +0300

    Added some icons

---
+++ To Do List Management App/To Do List Management App/Commands/RelayCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace To_Do_List_Management_App.Commands
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action commandTask;
+
+        private Predicate<object> canExecuteTask;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecuteTask != null && canExecuteTask(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            commandTask();
+        }
+    }
+}
