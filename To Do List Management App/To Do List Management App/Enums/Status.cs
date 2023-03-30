16f041d00226c33f0a78af2a218304e03518bde0
AlexUnguranu <a.ungureanu0099@gmail.com>
2023-03-30 10:43:59 +0300

    Added some enums for future features

---
+++ To Do List Management App/To Do List Management App/Enums/Status.cs
@@ -0,0 +1,12 @@
+﻿namespace To_Do_List_Management_App.Enums
+{
+    public enum Status
+    {
+        None,
+        NotStarted,
+        InProgress,
+        Completed,
+        OnHold,
+        Cancelled
+    }
+}
