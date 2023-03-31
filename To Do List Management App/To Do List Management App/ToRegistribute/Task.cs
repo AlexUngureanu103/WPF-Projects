abaf34990ed2934a20b04dc844ac443e82475eca
AlexUnguranu <a.ungureanu0099@gmail.com>
2023-03-31 09:08:18 +0300

    Configured startup page grids

--- To Do List Management App/To Do List Management App/ToRegistribute/Task.cs
+++ To Do List Management App/To Do List Management App/ToRegistribute/Task.cs
@@ -7,9 +7,13 @@ namespace To_Do_List_Management_App.ToRegistribute
     {
         public string Name { get; set; }
 
+        public string Description { get; set; }
+
         public Priority priority { get; set; }
 
         public TaskType type { get; set; }
+
+        public Status status { get; set; }
 
         public DateTime DueDate { get; set; }
     }
