cf121c2972d02880f33cd617698020be0904cdbd
AlexUnguranu <a.ungureanu0099@gmail.com>
2023-04-02 18:54:18 +0300

    Added it's dependencies

---
+++ To Do List Management App/To Do List Management App/Services/CategoryValidator.cs
@@ -0,0 +1,15 @@
+﻿namespace To_Do_List_Management_App.Services
+{
+    internal class CategoryValidator
+    {
+
+        public static bool CanExecuteAddCategory(string categoryName, string categoryDescription, string categoryImageSource)
+        {
+            if (string.IsNullOrEmpty(categoryName) || string.IsNullOrEmpty(categoryImageSource) || string.IsNullOrEmpty(categoryDescription))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ To Do List Management App/To Do List Management App/ViewModels/AddCategoryVM.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using To_Do_List_Management_App.Commands;
+using To_Do_List_Management_App.ResourceManagement;
+using To_Do_List_Management_App.Services;
+using To_Do_List_Management_App.ToRegistribute;
+
+namespace To_Do_List_Management_App.ViewModels
+{
+    internal class AddCategoryVM : BaseVM
+    {
+        private Category categoryToAdd;
+        public Category CategoryToAdd
+        {
+            get { return categoryToAdd; }
+            set
+            {
+                categoryToAdd = value;
+                OnPropertyChanged();
+            }
+        }
+        private bool canExecute;
+        public bool CanExecute
+        {
+            get { return canExecute; }
+            set
+            {
+                canExecute = value;
+            }
+        }
+
+        private AddCategoryCommands addCategoryCommands;
+
+        private List<string> categoryImageSources;
+        public List<string> CategoryImageSources
+        {
+            get { return categoryImageSources; }
+        }
+
+        private string categoryName;
+        public string CategoryName
+        {
+            get { return categoryName; }
+            set
+            {
+                categoryName = value;
+                canExecute = CategoryValidator.CanExecuteAddCategory(
+                    categoryName: categoryName,
+                    categoryDescription: categoryDescription,
+                    categoryImageSource: categoryImageSource
+                    );
+            }
+        }
+
+        private string categoryImageSource;
+        public string CategoryImageSource
+        {
+            get { return categoryImageSource; }
+            set
+            {
+                categoryImageSource = "\\"+value;
+                OnPropertyChanged();
+                canExecute = CategoryValidator.CanExecuteAddCategory(
+                    categoryName: categoryName,
+                    categoryDescription: categoryDescription,
+                    categoryImageSource: categoryImageSource
+                    );
+            }
+        }
+
+        private string categoryDescription;
+        public string CategoryDescription
+        {
+            get { return categoryDescription; }
+            set
+            {
+                categoryDescription = value;
+                canExecute = CategoryValidator.CanExecuteAddCategory(
+                    categoryName: categoryName,
+                    categoryDescription: categoryDescription,
+                    categoryImageSource: categoryImageSource
+                    );
+            }
+        }
+
+        private int categoryImageIndex;
+        public int CategoryImageIndex
+        {
+            get
+            {
+                return categoryImageIndex;
+            }
+            set
+            {
+                categoryImageIndex = value;
+            }
+        }
+
+        private ICommand addCategoryCommand;
+        public ICommand AddCategoryCommand
+        {
+            get
+            {
+                if (addCategoryCommand == null)
+                {
+                    addCategoryCommand = new RelayCommand(addCategoryCommands.AddCategoryCommand, param => CanExecute);
+                }
+                return addCategoryCommand;
+            }
+        }
+
+        private ICommand prevImageCommand;
+        public ICommand PrevImageCommand
+        {
+            get
+            {
+                if (prevImageCommand == null)
+                {
+                    prevImageCommand = new RelayCommand(addCategoryCommands.PrevImageIndexCommand, param => true);
+                }
+                return prevImageCommand;
+            }
+        }
+
+        private ICommand nextImageCommand;
+        public ICommand NextImageCommand
+        {
+            get
+            {
+                if (nextImageCommand == null)
+                {
+                    nextImageCommand = new RelayCommand(addCategoryCommands.NextImageIndexCommand, param => true);
+                }
+                return nextImageCommand;
+            }
+        }
+
+        public AddCategoryVM()
+        {
+            addCategoryCommands = new AddCategoryCommands(this);
+            categoryImageSources = new LoadImages(@"Images\CategoriesFolderIcons").ImagePaths;
+            categoryImageIndex = 0;
+        }
+    }
+}
