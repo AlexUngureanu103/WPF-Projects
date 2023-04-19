a00727cd5b957fd94384645f18c0cc2225e2fb30
AlexUnguranu <a.ungureanu0099@gmail.com>
2023-04-19 13:28:38 +0300

    Updated the filters class

--- To Do List Management App/To Do List Management App/Services/TaskSearchFilters.cs
+++ To Do List Management App/To Do List Management App/Services/TaskSearchFilters.cs
@@ -23,6 +23,20 @@ namespace To_Do_List_Management_App.Services
             return foundedTasks;
         }
 
+        public static ObservableCollection<TDTask> FindTasksByStatus(Status status, ObservableCollection<TDTask> tasksToFindIn)
+        {
+            ObservableCollection<TDTask> foundedTasks = new ObservableCollection<TDTask>(tasksToFindIn.Where(x => x.status == status));
+
+            return foundedTasks;
+        }
+
+        public static ObservableCollection<TDTask> FindTasksByType(TaskType type, ObservableCollection<TDTask> tasksToFindIn)
+        {
+            ObservableCollection<TDTask> foundedTasks = new ObservableCollection<TDTask>(tasksToFindIn.Where(x => x.type == type));
+
+            return foundedTasks;
+        }
+
         public static ObservableCollection<TDTask> FindTasksByDueDate(DateTime dueDate, ObservableCollection<TDTask> tasksToFindIn)
         {
             ObservableCollection<TDTask> foundedTasks = new ObservableCollection<TDTask>(tasksToFindIn.Where(x => DateTime.Compare(x.DueDate.Date, dueDate.Date) <= 0));
@@ -37,25 +51,26 @@ namespace To_Do_List_Management_App.Services
             return foundedTasks;
         }
 
-        public static ObservableCollection<TDTask> FindOverDueUncompletedTasks(ObservableCollection<TDTask> tasksToFindIn)
+        public static ObservableCollection<TDTask> FindUnCompletedTasks(ObservableCollection<TDTask> tasksToFindIn)
         {
-            ObservableCollection<TDTask> foundedTasks = new ObservableCollection<TDTask>(tasksToFindIn.Where(x => x.status != Status.Completed && DateTime.Compare(x.DueDate, DateTime.Now) <= 0));
+            ObservableCollection<TDTask> foundedTasks = new ObservableCollection<TDTask>(tasksToFindIn.Where(x => x.status != Status.Completed));
 
             return foundedTasks;
         }
 
-        public static ObservableCollection<TDTask> FindNotOverDueUncompletedTasks(ObservableCollection<TDTask> tasksToFindIn)
+        public static ObservableCollection<TDTask> FindOverDueTasks(ObservableCollection<TDTask> tasksToFindIn)
         {
-            ObservableCollection<TDTask> foundedTasks = new ObservableCollection<TDTask>(tasksToFindIn.Where(x => x.status != Status.Completed && DateTime.Compare(x.DueDate, DateTime.Now) > 0));
+            ObservableCollection<TDTask> foundedTasks = new ObservableCollection<TDTask>(tasksToFindIn.Where(x => DateTime.Compare(x.DueDate, DateTime.Now) <= 0));
 
             return foundedTasks;
         }
 
-        public static ObservableCollection<TDTask> FindTasksByStatus(Status status, ObservableCollection<TDTask> tasksToFindIn)
+        public static ObservableCollection<TDTask> FindNotOverDueTasks(ObservableCollection<TDTask> tasksToFindIn)
         {
-            ObservableCollection<TDTask> foundedTasks = new ObservableCollection<TDTask>(tasksToFindIn.Where(x => x.status == status));
+            ObservableCollection<TDTask> foundedTasks = new ObservableCollection<TDTask>(tasksToFindIn.Where(x => DateTime.Compare(x.DueDate, DateTime.Now) > 0));
 
             return foundedTasks;
         }
+
     }
 }
