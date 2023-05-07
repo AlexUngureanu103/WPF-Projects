881ebffa26956e93583ca21298b5e3dbd05140fe
AlexUnguranu <a.ungureanu0099@gmail.com>
2023-05-07 11:18:25 +0300

    small UI update for Admin Window

--- SchoolManagementApp/SchoolManagementApp/Views/LoginWindow.xaml.cs
+++ SchoolManagementApp/SchoolManagementApp/Views/LoginWindow.xaml.cs
@@ -2,7 +2,6 @@
 using SchoolManagementApp.ViewModels;
 using System;
 using System.Configuration;
-using System.Data.Entity;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -28,7 +27,6 @@ namespace SchoolManagementApp.Views
             _dbContext = new SchoolManagementDbContext(connectionString);
             LoginWindowVM = new LoginWindowVM(connectionString);
             InitializeComponent();
-
             DataContext = LoginWindowVM;
         }
 
@@ -50,9 +48,19 @@ namespace SchoolManagementApp.Views
             {
                 WindowContainer.Navigate(new ClassMasterUserControl(WindowContainer, _dbContext));
             }
+            UpdateMainWindowTitle($"School Management App - {LoginWindowVM.User.Role.AssignedRole} Control Panel");
             LoginWindowVM.Email = string.Empty;
             LoginWindowVM.Password = string.Empty;
             LoginWindowVM.User = null;
+        }
+
+        private void UpdateMainWindowTitle(string newTitle)
+        {
+            Window mainWindow = Window.GetWindow(this);
+            if (mainWindow != null)
+            {
+                mainWindow.Title = newTitle;
+            }
         }
     }
 }
